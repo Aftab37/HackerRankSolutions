7a16f217fd4c142f2cca27c41a532485d64a1890
Aftab37 <126570144+Aftab37@users.noreply.github.com>
2023-05-13 14:05:05 +0530

    Add files via upload

---
+++ Welcome_to_Java.java
@@ -0,0 +1,23 @@
+public class Welcome_to_Java {
+    public static void main(String[] args) {
+//            Input Format
+//            There is no input for this challenge.
+//
+//            Output Format
+//            You must print two lines of output:
+//
+//            Print Hello, World. on the first line.
+//            Print Hello, Java. on the second line.
+//
+//            Sample Output
+//            Hello, World.
+//            Hello, Java.
+
+
+            /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+            System.out.println("Hello, World.");
+            System.out.println("Hello, Java.");
+
+
+    }
+}
